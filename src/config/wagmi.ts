d58597d505b050fc028f0546822543759880a41a
faisalfarizi22 <faisal.alfariziak@gmail.com>
2025-10-23 10:11:15 +0700

    remove Walletconnect

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,14 +1,17 @@
 import { createConfig, http } from 'wagmi';
 import { base, baseSepolia } from 'wagmi/chains';
-import { farcasterMiniApp } from '@farcaster/miniapp-wagmi-connector';
+import { injected } from 'wagmi/connectors';
 
 export const wagmiConfig = createConfig({
   chains: [base, baseSepolia],
+  connectors: [
+    injected({
+      target: 'metaMask',
+    }),
+  ],
   transports: {
     [base.id]: http('https://mainnet.base.org'),
     [baseSepolia.id]: http('https://sepolia.base.org'),
   },
-  connectors: [
-    farcasterMiniApp(),
-  ],
+  ssr: false,
 });
